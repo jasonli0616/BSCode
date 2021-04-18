bd0ab4d2ff47f2b08b0ce11fbd56c44e6830ffd9
Jason Li <jasonli.0616@outlook.com>
2021-04-17 22:39:41 -0400

    New shortcuts, new menu bar, bug fixes

--- BSCode.py
+++ BSCode.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk as ttk
 from tkinter.filedialog import askopenfilename, askopenfile, askdirectory, asksaveasfile
+import tkinter.tix as tix
 from ttkthemes import ThemedTk
 import subprocess
 import os
@@ -25,11 +26,16 @@ root.configure(bg='#464646')
 s.configure("Title.TLabel", font=('*', 25), padding=10)
 s.configure("Subtitle.TLabel", font=('*', 17), padding=10)
 
+menubar = Menu(root)
+
 compilers = {
     '.py': 'python3',
     '.js': 'node',
     '.swift': 'swift',
 }
+
+if str(platform.system()) == 'Windows':
+    compilers['.py'] = 'python'
 
 fileChosen = False
 fileSaved = False
@@ -85,22 +91,10 @@ def newWin(
 def key_press(event):
     '''
     On key press event
+    '''
 
     '''
-    if str(platform.system()) == 'Darwin':
-        if str(event.state) == '8' and str(event.char) == 's':
-            saveBtnDo()
-
-        if str(event.state) == '8' and str(event.char) == 'r':
-            runBtnDo()
-
-    elif str(platform.system()) == 'Linux' or str(platform.system()) == 'Windows':
-        if str(event.state) == 'Control' and str(event.keysym) == 's':
-            saveBtnDo()
-
-        if str(event.state) == 'Control' and str(event.keysym) == 'c':
-            runBtnDo()
-
+    Autocomplete characters are unavailable until bugs are fixed
     if str(event.char) == '(':
         textInput.insert(INSERT, ')')
 
@@ -115,6 +109,7 @@ def key_press(event):
 
     if str(event.char) == '"':
         textInput.insert(INSERT, '"')
+    '''
 
     checkIsSaved()
 
@@ -128,8 +123,10 @@ def checkIsSaved():
     try:
         if fileLastSave != textInput.get(1.0, 'end-1c'):
             root.title(root_title + ' - ' + fileName + '*')
+            fileSaved = False
         else:
             root.title(root_title + ' - ' + fileName)
+            fileSaved = True
     except NameError:
         saveNewFileDo()
 
@@ -140,11 +137,14 @@ def saveNewFileDo():
     '''
     global fileName, filePath, filePathName, fileExt, fileChosen, fileSaved
 
-    savefileDialog = asksaveasfile(title='Save this file', initialdir='/', initialfile='Untitled.txt', defaultextension='.txt', filetypes=[('Text file', '*.txt'), ('Python', '*.py'), ('Node.js', '*.js'), ('Swift', '*.swift'), ('HTML', '*.html'), ('CSS', '*.css')])
-    fileName = str(os.path.split(savefileDialog)[1])
-    filePath = str(os.path.split(savefileDialog)[0])
-    filePathName = str(savefileDialog)
-    fileExt = str(os.path.splitext(fileName)[1])
+    savefileDialog = asksaveasfile(title='Save this file', initialdir='/', initialfile='Untitled.txt', defaultextension='.py', filetypes=[('All Files', '*.*'), ('Text file', '*.txt'), ('Python', '*.py'), ('Node.js', '*.js'), ('Swift', '*.swift'), ('HTML', '*.html'), ('CSS', '*.css')])
+    if str(savefileDialog.name) != '':
+        fileName = str(os.path.split(savefileDialog.name)[1])
+        filePath = str(os.path.split(savefileDialog.name)[0])
+        filePathName = str(savefileDialog.name)
+        fileExt = str(os.path.splitext(fileName)[1])
+        fileChosen = True
+        fileSaved = True
 
 
 def openFileBtnDo():
@@ -153,20 +153,21 @@ def openFileBtnDo():
     '''
     global fileName, filePath, filePathName, fileExt, fileChosen, fileSaved, fileLastSave
 
-    openFileDialog = askopenfilename(title='Select file to open', initialdir='/', filetypes=[('Text file', '*.txt'), ('Python', '*.py'), ('Node.js', '*.js'), ('Swift', '*.swift'), ('HTML', '*.html'), ('CSS', '*.css')])
-    fileName = str(os.path.split(openFileDialog)[1])
-    filePath = str(os.path.split(openFileDialog)[0])
-    filePathName = str(openFileDialog)
-    fileExt = str(os.path.splitext(fileName)[1])
-    root.title(root_title + ' - ' + fileName)
-    fileChosen = True
-    fileSaved = True
+    openFileDialog = askopenfilename(title='Select file to open', initialdir='/', filetypes=[('All Files', '*.*'), ('Text file', '*.txt'), ('Python', '*.py'), ('Node.js', '*.js'), ('Swift', '*.swift'), ('HTML', '*.html'), ('CSS', '*.css')])
+    if str(openFileDialog) != '':
+        fileName = str(os.path.split(openFileDialog)[1])
+        filePath = str(os.path.split(openFileDialog)[0])
+        filePathName = str(openFileDialog)
+        fileExt = str(os.path.splitext(fileName)[1])
+        root.title(root_title + ' - ' + fileName)
+        fileChosen = True
+        fileSaved = True
 
-    with open(filePathName) as f:
-        content = f.read()
-        textInput.delete('1.0', 'end')
-        textInput.insert('1.0', content)
-        fileLastSave = content
+        with open(filePathName) as f:
+            content = f.read()
+            textInput.delete('1.0', 'end')
+            textInput.insert('1.0', content)
+            fileLastSave = content
 
 
 def runBtnDo():
@@ -177,9 +178,9 @@ def runBtnDo():
 
     # Clear terminal
     if str(platform.system()) == 'Darwin' or str(platform.system()) == 'Linux':
-        subprocess.call(['clear'])
+        os.system('clear')
     elif str(platform.system()) == 'Windows':
-        subprocess.call(['cls'])
+        os.system('cls')
 
     # Initialize
     noRun = False
@@ -195,16 +196,12 @@ def runBtnDo():
     elif fileSaved == False:
         saveBtnDo()
         runBtnDo()
-
     elif fileExt == '.css' or fileExt == '.txt':
         noRun = True
         errorReason = f'This is a {fileExt} file'
 
     # Run file in terminal (or web browser for .html)
     else:
-        if str(platform.system()) == 'Windows':
-            compilers['.py'] = 'python'
-
         if fileExt != '.html':
             runList.append(compilers[fileExt])
 
@@ -246,16 +243,28 @@ def settingsBtnDo():
     '''
     Opens settings
     '''
-    global compilers
+
+    global compilsers, textInputFont
 
     def pyInterpreterSaveDo():
+        # Overwrite Python interpreter/compiler
+        global compilers
         compilers['.py'] = str(pyInterpreterEntry.get())
 
     def nodeInterpreterSaveDo():
+        # Overwrite Node.js interpreter/compiler
+        global compilers
         compilers['.js'] = str(nodeInterpreterEntry.get())
 
     def swiftInterpreterSaveDo():
+        # Overwrite Swift interpreter/compiler
+        global compilers
         compilers['.swift'] = str(swiftInterpreterEntry.get())
+
+    def fontNameSizeOverwriteDo():
+        # Overwrite font name
+        global textInput, textInputFont
+        textInput.config(font=(str(fontNameOverwriteEntry.get()), str(fontSizeVar.get())))
 
     # Initialize settings
     settingsWin = Toplevel()
@@ -267,15 +276,16 @@ def settingsBtnDo():
     # Create section to overwrite compiler
     compilerOverwriteFrame = ttk.Frame(settingsWin)
     compilerOverwriteFrame.pack()
-    compilerOverwriteTitle = ttk.Label(compilerOverwriteFrame, text='Overwrite default compiler/interpreter path', style='Subtitle.TLabel')
+    compilerOverwriteTitle = ttk.Label(compilerOverwriteFrame, text='Overwrite default compiler/interpreter', style='Subtitle.TLabel')
     compilerOverwriteTitle.pack()
 
     # Python overwrite
     pyInterpreterFrame = ttk.Frame(compilerOverwriteFrame)
     pyInterpreterFrame.pack()
-    pyInterpreterTitle = ttk.Label(pyInterpreterFrame, text='Python:')
+    pyInterpreterTitle = ttk.Label(pyInterpreterFrame, text='Python: ')
     pyInterpreterTitle.pack(side=LEFT)
     pyInterpreterEntry = ttk.Entry(pyInterpreterFrame)
+    pyInterpreterEntry.insert(1, compilers['.py'])
     pyInterpreterEntry.pack(side=LEFT)
     pyInterpreterSave = ttk.Button(pyInterpreterFrame, text='Save', command=pyInterpreterSaveDo)
     pyInterpreterSave.pack()
@@ -283,9 +293,10 @@ def settingsBtnDo():
     # Node.js overwrite
     nodeInterpreterFrame = ttk.Frame(compilerOverwriteFrame)
     nodeInterpreterFrame.pack()
-    nodeInterpreterTitle = ttk.Label(nodeInterpreterFrame, text='Node.js:')
+    nodeInterpreterTitle = ttk.Label(nodeInterpreterFrame, text='Node.js: ')
     nodeInterpreterTitle.pack(side=LEFT)
     nodeInterpreterEntry = ttk.Entry(nodeInterpreterFrame)
+    nodeInterpreterEntry.insert(1, compilers['.js'])
     nodeInterpreterEntry.pack(side=LEFT)
     nodeInterpreterSave = ttk.Button(nodeInterpreterFrame, text='Save', command=nodeInterpreterSaveDo)
     nodeInterpreterSave.pack()
@@ -293,12 +304,37 @@ def settingsBtnDo():
     # Swift overwrite
     swiftInterpreterFrame = ttk.Frame(compilerOverwriteFrame)
     swiftInterpreterFrame.pack()
-    swiftInterpreterTitle = ttk.Label(swiftInterpreterFrame, text='Swift:')
+    swiftInterpreterTitle = ttk.Label(swiftInterpreterFrame, text='Swift: ')
     swiftInterpreterTitle.pack(side=LEFT)
     swiftInterpreterEntry = ttk.Entry(swiftInterpreterFrame)
+    swiftInterpreterEntry.insert(1, compilers['.swift'])
     swiftInterpreterEntry.pack(side=LEFT)
     swiftInterpreterSave = ttk.Button(swiftInterpreterFrame, text='Save', command=swiftInterpreterSaveDo)
     swiftInterpreterSave.pack()
+
+
+
+    # Create section to overwrite font
+    fontOverwriteFrame = ttk.Frame(settingsWin)
+    fontOverwriteFrame.pack()
+    fontOverwriteTitle = ttk.Label(fontOverwriteFrame, text='Overwrite default font', style='Subtitle.TLabel')
+    fontOverwriteTitle.pack()
+
+    # Font name + size overwrite
+    fontNameSizeOverwriteFrame = ttk.Frame(fontOverwriteFrame)
+    fontNameSizeOverwriteFrame.pack()
+    fontNameOverwriteEntry = ttk.Entry(fontNameSizeOverwriteFrame)
+    fontNameOverwriteEntry.insert(1, textInputFont[0])
+    fontNameOverwriteEntry.pack(side=LEFT)
+    fontSizeVar = StringVar()
+    fontSizeOverwriteOptions = [str(textInputFont[1])]
+    for i in range(1, 1001):
+        fontSizeOverwriteOptions.append(str(i))
+    fontSizeVar.set(str(textInputFont[1]))
+    fontSizeOverwriteEntry = ttk.OptionMenu(fontNameSizeOverwriteFrame, fontSizeVar, *fontSizeOverwriteOptions)
+    fontSizeOverwriteEntry.pack(side=LEFT)
+    fontNameSizeOverwriteSave = ttk.Button(fontNameSizeOverwriteFrame, text='Save', command=fontNameSizeOverwriteDo)
+    fontNameSizeOverwriteSave.pack(side=LEFT)
 
 
 '''
@@ -318,7 +354,7 @@ titleFrame.pack()
 titleLab = ttk.Label(titleFrame, text="BSCode", style='Title.TLabel')
 titleLab.pack()
 
-# Text editor part
+# Text editor + Scroll bar
 textInputFrame = ttk.Frame(root)
 textInputFrame.pack()
 if str(platform.system()) == 'Windows':
@@ -330,7 +366,12 @@ else:
 scrollbar = Scrollbar(textInputFrame)
 scrollbar.pack(side=RIGHT, fill=Y)
 textInput = Text(textInputFrame, bg='#666666', fg='#F7F7F7', width=textInputWidth, height=textInputHeight, yscrollcommand=scrollbar.set)
-textInput.configure(font=("Courier", 12))
+if str(platform.system()) == 'Darwin':
+    textInputFont = ('Monaco', 12)
+elif str(platform.system()) == 'Windows' or str(platform.system()) == 'Linux':
+    textInputFont = ('Courier New', 12)
+textInput.insert(1.0, 'Welcome to the new version of BSCode!\n\nTo open a file, click "Open"\nTo make a new file, start typing\nTo change settings, click "Settings"\nTo run your file, click "Run in terminal"')
+textInput.configure(font=textInputFont)
 textInput.pack(side=LEFT)
 scrollbar.config(command=textInput.yview)
 
@@ -362,5 +403,41 @@ root.bind("<Key>", key_press)
 
 
 
+'''
+Menubar
+------------------------------
+'''
+
+# File menu
+if str(platform.system()) == 'Darwin':
+    openShortcut = 'Cmd+O'
+    newShortcut = 'Cmd+N'
+    saveShortcut = 'Cmd+S'
+    runShortcut = 'Cmd+R'
+elif str(platform.system()) == 'Windows' or str(platform.system()) == 'Linux':
+    openShortcut = 'Ctrl+O'
+    newShortcut = 'Ctrl+N'
+    saveShortcut = 'Ctrl+S'
+    runShortcut = 'Ctrl+R'
+
+
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label='Open', command=openFileBtnDo, accelerator=openShortcut)
+filemenu.add_command(label='New', command=saveNewFileDo, accelerator=newShortcut)
+filemenu.add_command(label='Save', command=saveBtnDo, accelerator=saveShortcut)
+menubar.add_cascade(label='File', menu=filemenu)
+
+runmenu = Menu(menubar, tearoff=0)
+runmenu.add_command(label='Run', command=runBtnDo, accelerator=runShortcut)
+menubar.add_cascade(label='Run', menu=runmenu)
+
+'''
+------------------------------
+'''
+
+
+
+
 if __name__ == '__main__':
+    root.config(menu=menubar)
     root.mainloop()
